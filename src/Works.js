4f567eceff5342053323edfa2921f80d43a3be96
spicysos <cchae216@gmail.com>
2019-12-22 22:59:45 -0500

    working on the works sections

--- src/Works.js
+++ src/Works.js
@@ -5,8 +5,6 @@ class Works extends Component {
 
     componentDidMount = () => {
         this.filterSelection("all");
-
-
         // Add active class to the current control button (highlight it)
         let btnContainer = document.getElementById("clearfix");
         var btns = btnContainer.getElementsByClassName("btn");
@@ -21,7 +19,7 @@ class Works extends Component {
     }
     // Hide elements that are not selected
     removeClass = (element, name) => {
-        var i, arr1, arr2;
+        let i, arr1, arr2;
         arr1 = element.className.split(" ");
         arr2 = name.split(" ");
         for (i = 0; i < arr2.length; i++) {
@@ -32,7 +30,7 @@ class Works extends Component {
         element.className = arr1.join(" ");
     }
     addClass = (element, name) => {
-        var i, arr1, arr2;
+        let i, arr1, arr2;
         arr1 = element.className.split(" ");
         arr2 = name.split(" ");
         for (i = 0; i < arr2.length; i++) {
@@ -85,33 +83,33 @@ class Works extends Component {
               <div id="portfolio-wrap">
                   <div className="container-box">
                       <div className="portfolio-overlay filter-div all react percussion">
-                          <div><p>Percussion Game</p></div>
+                          {/* <div><p>Percussion Game</p></div> */}
                           <i className="fas fa-drum icon"></i>
                       </div>
                       
                       <div className="portfolio-overlay filter-div all react weather">
-                        <div><p>Weather App</p></div>
+                        {/* <div><p>Weather App</p></div> */}
                         <i className="far fa-sun-cloud icon"></i>
                       </div>
                       <div className="portfolio-overlay filter-div all react todo">
-                        <div><p>To-Do App</p></div>
+                        {/* <div><p>To-Do App</p></div> */}
                         <i className="fas fa-clipboard-list icon"></i>
                       </div>
                       <div className="portfolio-overlay filter-div all react memory">
-                        <div><p>Memory Game </p></div>
+                        {/* <div><p>Memory Game </p></div> */}
                         <i className="fas fa-game-board icon"></i>
                       </div>
                       <div className="portfolio-overlay filter-div all vanilla color">
-                        <div><p>Color Game </p></div>
+                        {/* <div><p>Color Game </p></div> */}
                         <i className="fas fa-palette icon"></i>
                       </div>
                       <div className="portfolio-overlay filter-div all java pikachu">
-                        <div><p>Java Pikachu Game </p></div>
+                        {/* <div><p>Java Pikachu Game </p></div> */}
                         <i className="far fa-alien-monster icon"></i>
 
                       </div>
                       <div className="portfolio-overlay filter-div all react beer">
-                        <div><p>Online BeerShop</p></div>
+                        {/* <div><p>Online BeerShop</p></div> */}
                         <i className="fas fa-beer icon"></i>
                       </div>
 
